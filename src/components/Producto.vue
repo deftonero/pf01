ce889b676febb48dbfc8a80e6a46790f3121144b
deftonero <pabp86@hotmail.com>
2022-05-18 16:56:21 -0500

    proyecto final 01

---
+++ src/components/Producto.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<script>
+export default {
+  name: "Producto",
+  data() {
+    return {
+      producto: {
+        nombre: "",
+        precio: "",
+        descripcion: "",
+        imagen: "",
+        categoria: "",
+      },
+      categorias: [],
+      errors: [],
+    };
+  },
+};
+</script>
+
+<style></style>
